702d8fc725632e834ebee02a9b397a7cd02f3a0e
songjiefa <714737954@qq.com>
2018-10-22 09:32:39 +0800

    add ignore

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -4,8 +4,8 @@ $password = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
 $username = $domain + "\ucaadmin"
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
-#$password = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
-#$username = "\ucaadmin"
-#$lcredential = New-Object System.Management.Automation.PSCredential($username, $password)
+#$lpassword = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
+#$lusername = "\ucaadmin"
+#$lcredential = New-Object System.Management.Automation.PSCredential($lusername, $lpassword)
 
 Add-Computer -DomainName $domain -Credential $credential -Restart
