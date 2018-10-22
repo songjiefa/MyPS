ac901a901c596e6445a14039683173b9186a6622
songjiefa <714737954@qq.com>
2018-10-22 09:26:40 +0800

    update

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,2 +1,11 @@
-param([string]$input)
-Out-File -FilePath "c:\1.txt" -InputObject $input
+param ([string]$domain)
+
+$password = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
+$username = $domain + "\ucaadmin"
+$credential = New-Object System.Management.Automation.PSCredential($username, $password)
+
+#$password = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
+#$username = "\ucaadmin"
+#$lcredential = New-Object System.Management.Automation.PSCredential($username, $password)
+
+Add-Computer -DomainName $domain -Credential $credential -Restart
