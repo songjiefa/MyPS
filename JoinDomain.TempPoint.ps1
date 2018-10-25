048d8b03474056b7299807f8c8a73aab08140ecb
songjiefa <714737954@qq.com>
2018-10-26 00:41:24 +0800

    all2

--- JoinDomain.TempPoint.ps1
+++ JoinDomain.TempPoint.ps1
@@ -1,11 +1,11 @@
-param ([string]$domain="uca-e2016.wm.zhu.cn.qsft")
+param ([string]$domain)
 
 $password = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
 $username = $domain + "\ucaadmin"
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
-$lpassword = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
-$lusername = "\ucaadmin"
-$lcredential = New-Object System.Management.Automation.PSCredential($lusername, $lpassword)
+#$lpassword = 'I@mroot$PWD1' | ConvertTo-SecureString -asPlainText -Force
+#$lusername = "\ucaadmin"
+#$lcredential = New-Object System.Management.Automation.PSCredential($lusername, $lpassword)
 
 Add-Computer -DomainName $domain -Credential $credential -Restart
